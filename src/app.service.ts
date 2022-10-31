41b0674f43e7912a62dc4d229d56bf97c69c6603
easyhoon119forour <99165247+easyhoon119forour@users.noreply.github.com>
2022-10-31 21:13:56 +0900

    add test logic

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ExamType } from './type';
+
+let dummy: ExamType = [
+  {
+    name: 'jihoon',
+    email: 'jihoon@gmail.com',
+  },
+  {
+    name: 'jay',
+    email: 'jay@gmail.com',
+  },
+];
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): ExamType {
+    return dummy;
+  }
+
+  putEmail(name: string): string {
+    dummy = dummy.map((item) => {
+      return item.name === name
+        ? { ...item, email: `${item.name}@naver.com` }
+        : item;
+    });
+    return name;
   }
 }
